fd5fab6b0a09f2d09b87f9d12b7722b02ee93062
Joshua Brunhuber <joshua.brunhuber@gmail.com>
2018-06-02 18:36:46 +0200

    Initial Commit

---
+++ NeuralNet/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NeuralNet
+//
+//  Created by Joshua Brunhuber on 02.06.18.
+//  Copyright © 2018 Joshua Brunhuber. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
